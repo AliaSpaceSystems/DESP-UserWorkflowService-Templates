58bf70baca781fec6aafcc2928f211f0f07cac34
carcari <cristinaarcari@alia-space.com>
2024-10-09 02:05:50 +0200

    DEA Notebook for creating a new story showing user assets generated from DESP cache with DEA API

---
+++ dea/data2story/get_era5_t2m_from_cacheb.py
@@ -0,0 +1,39 @@
+import xarray as xr
+
+# output filename
+filename = 't2m_era5_20240801.nc'
+
+# retrieve era5 single level data from cacheb. Dataset of interest: t2m (t metre temperature)
+data = xr.open_dataset(
+        "https://cacheb.dcms.destine.eu/era5/reanalysis-era5-single-levels-v0.zarr",
+        engine="zarr",
+        storage_options={"client_kwargs": {"trust_env": "true"}},
+        chunks={},
+    )
+
+# Convert to Celsius
+
+t2m = data.t2m.astype("float32") - 273.15
+t2m.attrs["units"] = "C"
+
+t2m = t2m.assign_coords(longitude=(t2m['longitude'] % 360))  # First ensure longitude is in 0-360 range
+t2m['longitude'] = xr.where(t2m['longitude'] > 180, t2m['longitude'] - 360, t2m['longitude'])  # Then shift to -180 to 180
+
+# Sort by longitude to maintain the proper order
+t2m = t2m.sortby('longitude')
+
+# t2m['longitude'] = xr.where(t2m['longitude'] > 180, t2m['longitude'] - 360, t2m['longitude'])
+
+
+
+# get a single time step
+t2m_slice = t2m.sel(valid_time="2024-08-01T12:00:00")
+
+print(f"Data to download...")
+print(t2m_slice)
+
+t2m_slice = t2m_slice.compute()
+# save as NetCDF
+t2m_slice.to_netcdf(filename)
+
+print(f"Your file {filename} is ready!")
